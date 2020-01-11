bc96067861974b961a2b0d9a6bc691a4bb507acf
test111qqq <danlee0150@gmail.com>
2020-01-11 21:08:38 +0800

    Retrofit Interface Methods

--- app/src/main/java/com/example/foodrecipes2/request/RecipeAPI.java
+++ app/src/main/java/com/example/foodrecipes2/request/RecipeAPI.java
@@ -1,4 +1,26 @@
 package com.example.foodrecipes2.request;
 
-public interface RecipeAPI  {
+import com.example.foodrecipes2.request.responses.RecipeResponse;
+import com.example.foodrecipes2.request.responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface RecipeAPI {
+
+	// SEARCH
+	@GET("api/search")
+	Call<RecipeSearchResponse> searchRecipe(
+		@Query("key") String key,
+		@Query("q") String query,
+		@Query("page") String page
+	);
+
+	// GET RECIPE
+	@GET("api/get")
+	Call<RecipeResponse> getRecipe(
+		@Query("key") String key,
+		@Query("rId") String recipe_id
+	);
 }
