90296535a66d5328cb246ddf8bcee58d92701759
test111qqq <danlee0150@gmail.com>
2020-01-11 21:54:07 +0800

    Enable Internet Access for this app, Testing Retrofit Response Objects

--- app/src/main/java/com/example/foodrecipes2/RecipeListActivity.java
+++ app/src/main/java/com/example/foodrecipes2/RecipeListActivity.java
@@ -1,9 +1,26 @@
 package com.example.foodrecipes2;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.example.foodrecipes2.models.Recipe;
+import com.example.foodrecipes2.request.RecipeAPI;
+import com.example.foodrecipes2.request.ServiceGenerator;
+import com.example.foodrecipes2.request.responses.RecipeResponse;
+import com.example.foodrecipes2.request.responses.RecipeSearchResponse;
+import com.example.foodrecipes2.util.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class RecipeListActivity extends BaseActivity {
+
+	private static final String TAG = "RecipeListActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,12 +30,66 @@ public class RecipeListActivity extends BaseActivity {
 		findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				if (mProgressBar.getVisibility() == View.VISIBLE) {
-					showProgressBar(false);
+				testRetrofitRequest();
+			}
+		});
+	}
+
+	private void testRetrofitRequest() {
+		RecipeAPI recipeAPI = ServiceGenerator.getRecipeAPI();
+
+//		Call<RecipeSearchResponse> responseCall = recipeAPI
+//				.searchRecipe(
+//						Constants.API_KEY,
+//						"chicken breast",
+//						"1"
+//						);
+//
+//		responseCall.enqueue(new Callback<RecipeSearchResponse>() {
+//			@Override
+//			public void onResponse(Call<RecipeSearchResponse> call, Response<RecipeSearchResponse> response) {
+//				Log.d(TAG, "onResponse: server response" + response.toString());
+//				if (response.code() == 200) {
+//					Log.d(TAG, "onResponse: " + response.body().toString());
+//					List<Recipe> recipes = new ArrayList<>(response.body().getRecipes());
+//					for (Recipe recipe: recipes) {
+//						Log.d(TAG, "onResponse: " + recipe.getTitle());
+//					}
+//				}
+//				else {
+//					Log.d(TAG, "onResponse: " + response.errorBody().toString());
+//				}
+//			}
+//
+//			@Override
+//			public void onFailure(Call<RecipeSearchResponse> call, Throwable t) {
+//
+//			}
+//		});
+
+		Call<RecipeResponse> responseCall = recipeAPI
+				.getRecipe(
+						Constants.API_KEY,
+						"30373"
+				);
+
+		responseCall.enqueue(new Callback<RecipeResponse>() {
+			@Override
+			public void onResponse(Call<RecipeResponse> call, Response<RecipeResponse> response) {
+				Log.d(TAG, "onResponse: server response" + response.toString());
+				if (response.code() == 200) {
+					Log.d(TAG, "onResponse: " + response.body().toString());
+					Recipe recipe = response.body().getRecipe();
+					Log.d(TAG, "onResponse: " + recipe.getTitle());
 				}
 				else {
-					showProgressBar(true);
+					Log.d(TAG, "onResponse: " + response.errorBody().toString());
 				}
+			}
+
+			@Override
+			public void onFailure(Call<RecipeResponse> call, Throwable t) {
+
 			}
 		});
 	}
