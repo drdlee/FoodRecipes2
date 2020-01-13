6136e4bffa5c1786fda591d99e1d328cae5412b2
test111qqq <danlee0150@gmail.com>
2020-01-13 14:09:41 +0800

    Make AppExecutor to run background thread,  use AppExecutor in RecipeApiClient

--- app/src/main/java/com/example/foodrecipes2/request/RecipeApiClient.java
+++ app/src/main/java/com/example/foodrecipes2/request/RecipeApiClient.java
@@ -3,9 +3,13 @@ package com.example.foodrecipes2.request;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.foodrecipes2.AppExecutors;
 import com.example.foodrecipes2.models.Recipe;
+import com.example.foodrecipes2.util.Constants;
 
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class RecipeApiClient {
 
@@ -25,5 +29,24 @@ public class RecipeApiClient {
 
 	public LiveData<List<Recipe>> getRecipes() {
 		return mRecipes;
+	}
+
+	public void searchRecipeApi() {
+		final Future handler = AppExecutors.getInstance().networkIO().submit(new Runnable() {
+			@Override
+			public void run() {
+				// retrieve data from rest api
+//				mRecipes.postValue();
+			}
+		});
+
+		// below code will fun after 3000ms / 3sec to cancel the Future handler created above
+		AppExecutors.getInstance().networkIO().schedule(new Runnable() {
+			@Override
+			public void run() {
+				//Let the user know its time out
+				handler.cancel(true);
+			}
+		}, Constants.NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
 	}
 }
